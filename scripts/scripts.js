5d9c6373aedaa7275e945c3718f8a184a9cc4a40
quasarzet <quasarzet@hotmail.com>
2021-02-18 12:04:50 -0500

    Third Commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,19 +5,16 @@ const body = document.body;
 const line1 = document.querySelector('.line-1');
 const line2 = document.querySelector('.line-2');
 const line3 = document.querySelector('.line-3');
+const cross = document.querySelector('.svg-cross');
 
 burgerMenu.timeline = gsap.timeline({paused:true});
 burgerMenu.timeline
-.to('.line-1', {x:-25, duration: 0.2, opacity: 0})
-.to('.line-3', {x:25, duration: 0.2, opacity: 0}, "-=0.2")
-.to('.line-1', {x:25, duration: 0.1, opacity: 0})
-.to('.line-3', {x:-25, duration: 0.1, opacity: 0})
-.to('.line-2', {duration: 0.1, opacity: 0}, "-=0.2")
-.to('.line-1', {opacity: 0, transform:"rotateZ(-45deg)", transformOrigin: "right", translateY: '-1px', duration: 0.1})
-.to('.line-3', {opacity: 0, transform:"rotateZ(45deg)", transformOrigin: "right", translateY: '4px', duration: 0.1})
-.to(['.line-1', '.line-3'], {duration: 0.1, opacity: 1},)
+.to(line1, {x:-40, duration: 0.2, opacity: 0})
+.to(line3, {x:40, duration: 0.2, opacity: 0}, "-=0.2")
+.to(line2, {duration: 0.1, opacity: 0}, "-=0.2")
 .to(overlay, {left: 0, duration: 0.1},"-=0.3")
 .to(overlay, {opacity: 1, duration: 0.3})
+.from(cross, {opacity: 0, scale: 0.5, duration: 0.5})
 
 burgerMenu.addEventListener('click', ()=>{
     body.classList.toggle('hide-scrolling');
